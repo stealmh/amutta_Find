ce9ec240eee23d1e49764349114ae4a0c14647ec
stealmh <kmh922@naver.com>
2022-12-20 20:27:19 +0900

    delete file

--- amutta/Models/Constants.swift
+++ amutta/Models/Constants.swift
@@ -9,21 +9,12 @@
 import Foundation
 // MARK: Name Space
 
-//데이터 영역에 저장이 됨
-//사용하게 될 API 문자열 묶음
-public enum MusicApi {
-    static let requestUrl = "https://itunes.apple.com/search?"
-    static let mediaParam = "media=music"
-}
-
 // 사용하게될 Cell 문자열 묶음
 public struct Cell {
     static let musicCellIdentifier = "MusicCell"
     static let musicCollectionViewCellIdentifier = "CollectionViewCell"
     private init() {}
 }
-
-
 
 // 컬렉션뷰 구성을 위한 설정
 public struct CVCell {
